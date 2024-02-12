100787f797ab815e98bef22d56535aa0e377c848
= <=>
2024-02-12 14:52:07 +0000

    Added reduce method

---
+++ Arrays/reduce.js
@@ -0,0 +1,14 @@
+/* 
+The reduce() method of Array instances executes a user-supplied "reducer" callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. The final result of running the reducer across all elements of the array is a single value.
+*/
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce((accumulator, currentValue, currentIndex) => {
+  return accumulator + currentValue;
+}, initialValue);
+
+console.log(sumWithInitial);
+// Expected output: 10
