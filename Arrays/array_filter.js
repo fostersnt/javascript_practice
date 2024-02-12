5fd1982f52ff5bea982f0ee713b0f0c084dbc76b
= <=>
2024-02-12 12:51:49 +0000

    Grouped array files in one directory

---
+++ Arrays/array_filter.js
@@ -0,0 +1,13 @@
+
+
+//Filters the given to select all the numbers that leaves a remainder of 0 when divided by 2
+function check_even(arr) {
+   let result = arr.filter((a)=>{
+        return a%2 == 0;
+    })
+    return result;
+}
+
+let new_array = [1, 0, -4, 9, 2];
+
+console.log(check_even(new_array));
