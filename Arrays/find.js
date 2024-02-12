7d866422838c6c81431052465ff9e620b6199815
= <=>
2024-02-12 14:15:17 +0000

    Added a comment

--- Arrays/find.js
+++ Arrays/find.js
@@ -1,4 +1,6 @@
-const data = [3, 0, 34, 21, 6];
+//find() method returns the first matching element of the array. The find() takes-in a closure
+
+const data = [3, 0, 34, 21, 6, 67];
 
 let found = data.find((element) => element > 20);
 
