8b86886e3690d7c54a7c9e670edbf519231654b4
= <=>
2024-02-12 14:37:17 +0000

    Added from.js

---
+++ Arrays/from.js
@@ -0,0 +1,6 @@
+//The from() can take-in a closure as the second argument
+
+console.log(Array.from('Foster'));
+// Expected output: Array ["f", "o", "o"]
+
+// console.log(Array.from([1, 2, 3], (x) => x + x));
